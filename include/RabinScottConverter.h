0ec6e7e2a154631da31ec55f0b52cf15b9ccad43
saintninja <stas.saintninja@gmail.com>
2026-03-08 19:04:57 +0300

    fix: add RabinScottConverter move_by_char declaration

--- include/RabinScottConverter.h
+++ include/RabinScottConverter.h
@@ -6,6 +6,7 @@ private:
     std::set<int> epsilonClosure(const NFA& nfa, const std::set<int>& states);
     std::set<int> epsilonClosure(const NFA& nfa, int state);
     bool containsAcceptState(const NFA& nfa, const std::set<int>& states);
+    std::set<int> move_by_char(const NFA& nfa, const std::set<int>& states, char symbol);
 
 public:
     DFA operator()(const NFA& nfa, const std::set<char>& alphabet);
